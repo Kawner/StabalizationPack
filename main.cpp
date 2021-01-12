27cf6ae56932ad0ce0b646c10169b2d2816eaad4
MilesYoung <myoung39@calpoly.edu>
2021-01-12 13:51:37 -0800

    Miles's initial test commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "mbed.h"
+
+// class motor defines a motor object
+class motor {
+    public:
+        int motorNum;
+        int pinIN1;
+        int channel1;
+        int pinIN2;
+        int channel2;
+        int timNum;
+
+    
+
+
+};
+
+
+// main() runs in its own thread in the OS
+int main()
+{
+  
+    return 0;
+}
+
