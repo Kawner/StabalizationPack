77de8803de3f1d76deee0cbe8aeb03eb73765a6d
MilesYoung <myoung39@calpoly.edu>
2021-01-14 14:14:56 -0800

    secondary commit

--- main.cpp
+++ main.cpp
@@ -4,22 +4,29 @@
 class motor {
     public:
         int motorNum;
-        int pinIN1;
-        int channel1;
-        int pinIN2;
-        int channel2;
-        int timNum;
+        pin
+        PwmOut led(int pinA);
 
-    
-
-
+    void setDuty(float duty){
+        
+        // sets the duty cycle for vibrational motor PWM
+        digitalWrite(pin,duty);
+    };
 };
 
 
 // main() runs in its own thread in the OS
 int main()
 {
-  
+    // create motor object
+    motor motor1;
+    // assign motor number
+    motor1.motorNum = 1;
+    // assign motor pin
+    motor1.pinA = LED1;
+    // set motor duty cycle
+    motor1.setDuty(50);
+
     return 0;
-}
+};
 
