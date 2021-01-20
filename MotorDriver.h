88bc64c12f3610e666d3b99fbded5b4021775c7f
MilesYoung <myoung39@calpoly.edu>
2021-01-19 21:07:51 -0800

    secondary commit

--- MotorDriver.h
+++ MotorDriver.h
@@ -4,6 +4,7 @@
 class motor {
     public:
         int motorNum;
+        timer ;
         pin
         PwmOut led(int pinA);
 
@@ -13,20 +14,3 @@ class motor {
         digitalWrite(pin,duty);
     };
 };
-
-
-// main() runs in its own thread in the OS
-int main()
-{
-    // create motor object
-    motor motor1;
-    // assign motor number
-    motor1.motorNum = 1;
-    // assign motor pin
-    motor1.pinA = LED1;
-    // set motor duty cycle
-    motor1.setDuty(50);
-
-    return 0;
-};
-
