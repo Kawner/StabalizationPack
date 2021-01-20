7c71153c1a38880ce7e6da005f77839cf7d1e021
MilesYoung <myoung39@calpoly.edu>
2021-01-19 21:10:05 -0800

    Still trying to figure out how to commit properly

--- main.cpp
+++ main.cpp
@@ -1,19 +1,5 @@
 #include "mbed.h"
-
-// class motor defines a motor object
-class motor {
-    public:
-        int motorNum;
-        pin
-        PwmOut led(int pinA);
-
-    void setDuty(float duty){
-        
-        // sets the duty cycle for vibrational motor PWM
-        digitalWrite(pin,duty);
-    };
-};
-
+#include "MotorDriver.h"
 
 // main() runs in its own thread in the OS
 int main()
