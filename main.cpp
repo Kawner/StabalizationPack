20409e223f6e1373314ca14eddbb807c794f329b
MilesYoung <myoung39@calpoly.edu>
2021-01-28 14:10:30 -0800

    Reset main

--- main.cpp
+++ main.cpp
@@ -1,23 +1,9 @@
 #include "mbed.h"
-#include"MAX32630FTHR_PwmOut.h"
-// #include "MotorDriver.h"
-MAX32630FTHR_PwmOut led = MAX32630FTHR_PwmOut(LED2);
+#include "MAX32630FTHR_PwmOut.h"
+#include "MotorDriver.h"
  
  int main()
  {
-     float dc;
-     unsigned int idx = 0;
- 
-     while (1) {
-         for (dc = 0.0f; dc <= 1.0f; dc += 0.1f) {
-             led.write(dc);
-             wait_us(idx*1000000);
-         }
-         idx++;
-
-         if(idx == 10){
-             idx = 0;
-         }
-    }
+    
     return 0;
-};
+}
